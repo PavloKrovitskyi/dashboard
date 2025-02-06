f37988893bc1ee4874a571250ed8cee5d471f6d2
Pavlo K <pkrovickij@gmail.com>
2025-02-06 01:05:15 +0100

    first commit

---
+++ dist/js/min/script.js
@@ -0,0 +1,1 @@
+"use strict";const menuIcon=document.querySelector(".icon-menu");menuIcon.addEventListener("click",(function(e){document.documentElement.classList.toggle("menu-open"),e.target.closest(".menu__link")&&document.documentElement.classList.remove("menu-open")}));
